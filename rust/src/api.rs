560128fb421f1b83af1af20003c92a7c799a5acb
Michael Martis <michaeljmartis@gmail.com>
2023-04-12 16:10:59 +1000

    Finished translating API to rust.

--- rust/src/api.rs
+++ rust/src/api.rs
@@ -147,3 +147,96 @@ pub enum State {
     // Some other in-game error (e.g. tried to play an invalid card).
     MatchError(String), // Reason.
 }
+
+// Static info.
+
+// Background information about the lobby.
+#[derive(Debug)]
+pub struct LobbyContext {
+  // Number of players currently joined.
+  pub players_count: isize,
+
+  // Your index in the player list.
+  pub your_player_index: isize,
+
+  // Your index in the team list (i.e. in [0, 1]).
+  pub your_team_index: isize,
+}
+
+// Background information about the match.
+#[derive(Debug)]
+pub struct MatchContext {
+  // Game history. TODO make more sophisticated.
+  //   (team 1 score delta, team 1 score total, 
+  //    team 2 score delta, team 2 score total).
+  pub past_games: Vec<(isize, isize, isize, isize)>,
+}
+
+// Background information about the bidding.
+#[derive(Debug)]
+pub struct BiddingContext {
+  // The last bids made by each player. Ordered from player 1 to player 4.
+  pub bids: Vec<Option<Bid>>,  // Invariant: length of 4.
+  
+  pub current_bidder_index: isize,
+}
+
+// Background information about the bid that won.
+#[derive(Debug)]
+pub struct WinningBidContext {
+  pub winning_bidder_index: isize,
+
+  pub winning_bid: Bid,  // Invariant: not a Pass.
+}
+
+// Background information about the tricks being played.
+#[derive(Debug)]
+pub struct PlaysContext {
+  // The joker suit, if fixed.
+  pub joker_suit: Option<Suit>,
+
+  // The number of tricks you have won.
+  pub your_tricks_count: isize,
+
+  // The number of tricks they have won. 
+  pub their_tricks_count: isize,
+
+  // The number of cards in each player's hand.
+  pub hand_sizes: Vec<isize>,  // Invariant: length of 4.
+
+  // The previous trick, if there was one. Listed in order from player 1 to
+  // player 4. Inner Option is to support e.g. miz bids, where one player
+  // doesn't play.
+  pub previous_trick: Option<Vec<Option<Play>>>,
+
+  // The ongoing trick. Listed in order from player 1 to player 4.
+  pub current_trick: Vec<Option<Play>>,
+
+  // Index in the player list of the currently-playing player.
+  pub currently_playing_player_index: isize,
+}
+
+// Background information about the current game (i.e. the current bidding,
+// bidding-won, hands played cycle).
+#[derive(Debug)]
+pub struct GameContext {
+  // The cards in your hand.
+  pub hand: Vec<Card>,
+
+  pub bidding_context: BiddingContext,
+
+  pub winning_bid_context: Option<WinningBidContext>,
+
+  pub plays_context: Option<PlaysContext>,
+}
+
+// Background information about the session. Sub-contexts are populated as they
+// become valid.
+#[derive(Debug)]
+pub struct Context {
+  pub lobby_context: LobbyContext,
+
+  pub match_context: Option<MatchContext>,
+
+  pub game_context: Option<GameContext>,
+}

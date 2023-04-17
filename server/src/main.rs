ad657775530fe56a26e9b24e70b8fb2111e81e40
Michael Martis <martisdev@gmail.com>
2023-04-17 10:18:32 +0000

    Switched to using Tokio.
    Otherwise, ownership issues will eventually arise because we can't have two
    different objects owning the reading and writing ends of a web socket.
    
    Also introduced a channel-based abstraction that is closer to how the game
    engine will eventually consume messages from clients.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,53 +1,95 @@
+use futures_util::SinkExt;
+use futures_util::StreamExt;
 use std::env;
-use std::net;
-use std::sync::mpsc;
-use std::thread;
+use std::io;
 
-use tungstenite as ws2;
+use tokio::sync::mpsc;
+use tokio_tungstenite as tokio_ws2;
+use tokio_ws2::tungstenite as ws2;
 
-// Send WebSocket messages from the given TCP stream through the given channel. Forms the basis of
-// logic that will send messages from web clients to the game engine.
-fn channel_ws_msgs(stream: Result<net::TcpStream, std::io::Error>, tx: mpsc::Sender<ws2::Message>) {
-    if stream.is_err() {
-        return;
-    }
+type Sender = mpsc::UnboundedSender<ws2::Message>;
+type Receiver = mpsc::UnboundedReceiver<ws2::Message>;
 
-    let mut websocket = match ws2::accept(stream.unwrap()) {
-        Err(_) => return,
-        Ok(v) => v,
-    };
+// Returns a sender that transmits messages to one WebSocket client, and a receiver that receives
+// messages from that same client. Forms the basis of logic that will eventually handle game engine
+// <-> web client communication.
+fn init_echo_socket(
+    websocket: tokio_ws2::WebSocketStream<tokio::net::TcpStream>,
+) -> (Sender, Receiver) {
+    let (mut write, mut read) = websocket.split();
 
-    loop {
-        let msg = match websocket.read_message() {
-            Err(_) => break,
-            Ok(v) => v,
-        };
-
-        if !msg.is_text() {
-            continue;
+    // Thread consuming messages transmitted from the server.
+    let (out_tx, mut out_rx) = mpsc::unbounded_channel();
+    tokio::spawn(async move {
+        loop {
+            if let Some(msg) = out_rx.recv().await {
+                if write.send(msg).await.is_err() {
+                    println!("Failed to reply to client");
+                    return;
+                }
+            } else {
+                println!("Connection closed by server");
+                return;
+            }
         }
+    });
 
-        tx.send(msg).unwrap();
-    }
+    // Thread transmitting messages received through the web socket.
+    let (in_tx, in_rx) = mpsc::unbounded_channel();
+    tokio::spawn(async move {
+        loop {
+            if let Some(result) = read.next().await {
+                if result.is_ok() && in_tx.send(result.unwrap()).is_err() {
+                    println!("Failed to consume client message");
+                    return;
+                }
+            } else {
+                println!("Connection closed by client");
+                return;
+            }
+        }
+    });
+
+    (out_tx, in_rx)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
-    // Create a channel and print its messages on another thread.
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        for m in rx {
-            println!("{}", m);
-        }
-    });
+    // Listen for TCP requests incoming to the given address.
+    let listener = tokio::net::TcpListener::bind(addr.clone()).await?;
+    println!("Listening on: {addr}");
 
-    // Send messages from all incoming WebSocket connections to the channel.
-    let server = net::TcpListener::bind(addr).unwrap();
-    for stream in server.incoming() {
-        let tx2 = tx.clone();
-        thread::spawn(move || channel_ws_msgs(stream, tx2));
+    loop {
+        // Establish the TCP connection.
+        let Ok((stream, client_addr)) = listener.accept().await else { continue };
+
+        // Establish the WebSocket connection.
+        let Ok(websocket) = tokio_ws2::accept_async(stream).await else {continue };
+        println!("New WebSocket connection: {client_addr}");
+
+        tokio::spawn(async move {
+            // Get handles to listen and reply to the given client.
+            let (out_tx, mut in_rx) = init_echo_socket(websocket);
+
+            loop {
+                if let Some(msg) = in_rx.recv().await {
+                    if !msg.is_text() && !msg.is_binary() {
+                        continue;
+                    }
+
+                    if let Err(mpsc::error::SendError(_)) = out_tx.send(msg) {
+                        println!("Failed to communicate with sending thread");
+                        break;
+                    }
+                } else {
+                    println!("Failed to communicate with receiving thread");
+                    break;
+                }
+            }
+        });
     }
 }
